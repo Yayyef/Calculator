b42813d4a9aa8f03124e1df5b8150aba2789750d
Yayyef <64088247+Yayyef@users.noreply.github.com>
2020-10-22 17:26:57 +0200

    My event delegation system seems to be working

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 let result;
+let displayValue = "";
+const display = document.querySelector('.display');
 const allButtons = document.querySelector('.allButtons');
 
 allButtons.onclick = function(e) {
-    let target = e.target;
-    console.log(target.textContent);
+    let targetButton = e.target;
+    // CHECKS IF TARGET IS A BUTTON
+    if (!targetButton.matches('button')) {
+        return
+    } else if (0 <= targetButton.dataset.index && targetButton.dataset.index <= 9){
+    displayValue += targetButton.textContent;
+    display.innerHTML = displayValue;
+    }
+    console.log(typeof(targetButton.dataset.index));
 };
 
 function operate(firstNumber, secondNumber) {
     return result = firstNumber + secondNumber;
+};
+
+function clear() {
+    display.innerHTML = "";
 }
-
-// function captureButton(event) {
-//     console.log(this);
-
-//     console.log(event.target);
-// }
