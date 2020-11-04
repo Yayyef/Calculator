cd1affd9e6d39549d7fbea9e3f46bdf107dad546
Yayyef <64088247+Yayyef@users.noreply.github.com>
2020-11-04 12:18:59 +0100

    Update() et probleme de conditionnalité sur les opérateurs réglé

--- script.js
+++ script.js
@@ -7,10 +7,11 @@ let operator2 = '';
 // Sert a reset le display seulement si un operator a été défini.
 let operatorSet;
 
-let displayValue = "";
+let displayValue = '';
 const display = document.querySelector('.display');
 const allButtons = document.querySelector('.allButtons');
 
+// J'utilise l'event delegation.
 allButtons.onclick = function(e) {
     let targetButton = e.target;
     let buttonValue = targetButton.dataset.index;
@@ -24,7 +25,7 @@ allButtons.onclick = function(e) {
         if (operatorSet === true) {
             clear();
             operatorSet = false;
-        }
+        };
 
         displayValue += buttonValue;
         display.innerHTML = displayValue;
@@ -34,50 +35,86 @@ allButtons.onclick = function(e) {
 
     } else if (buttonValue === '+' || buttonValue === '-' || buttonValue === '*' || buttonValue === '/') {
 
-        // operator1 = buttonValue;
-        operatorSet = true;
-
-        if (num1 === '') {
+        // PREMIER OPERATEUR, phase d'initialisation
+        if (num1 === '' && displayValue !== '') {
+            operatorSet = true;
             operator1 = buttonValue;
             num1 = parseInt(displayValue);
+        
+        } else if (num1 !== '' && num2 === '' && operator1 !== '') {
+            // Pour éviter un bug si on clique deux fois sur des operateurs.
+            if (operatorSet === true) {
+                operator1 = buttonValue;
 
-        } else if (num1 !== '' && num2 === '') {
-            operator2 = buttonValue;
-            num2 = parseInt(displayValue);
-            operate(num1, num2, operator1);
-            operator1 = '';
-            displayValue = result;
-            display.innerHTML = displayValue;
-            num1 = displayValue;
-            // num2 = '';
+            } else if (operatorSet === false) {                
+                operator2 = buttonValue;
+                num2 = parseInt(displayValue);
+                operate(num1, num2, operator1);
+                updateDisplay();
+                num2 = '';
+                operator1 = '';
+                
+            };
+        
+        } else if (operator2 !== '') {
+            // Pour éviter un bug si on clique deux fois sur des operateurs.
+            if (operatorSet === true) {
+                operator1 = buttonValue;
 
-        } else if (operator2 !== '') {            
-            num2 = parseInt(displayValue);
-            operate(num1, num2, operator2);
-            displayValue = result;
-            display.innerHTML = displayValue;
-            num1 = parseInt(displayValue);
-            num2 = '';
-            operator2 = '';
-            operator1 = buttonValue;
-        }
+            } else if (operatorSet === false) {                
+                operator1 = buttonValue;            
+                num2 = parseInt(displayValue);
+                operate(num1, num2, operator2);
+                updateDisplay();
+                num2 = '';
+                operator2 = '';
+                
+            };   
+
+        };
+
+        console.log(`1 ${operator1}`);  
+        console.log(`2 ${operator2}`);
 
     } else if (buttonValue === '=') {
 
-        num2 = parseInt(displayValue);
-        operate(num1, num2, operator1);
-        displayValue = result;
-        display.innerHTML = displayValue;
-        num1 = parseInt(displayValue);
-        num2 = '';
-        // operator1 = undefined;
-        
+        if (operator2 === '') {
 
-        console.log(`Num1 is ${num1}`);
-        console.log(`Num2 is ${num2}`);
-        console.log(`Operator1 is ${operator1}`);
-    }
+            if (operatorSet === true) {
+                operator1 = '';
+                return; 
 
+            } else if (operatorSet === false) {               
+                num2 = parseInt(displayValue);
+                operate(num1, num2, operator1);
+                updateDisplay();
+                num2 = '';
+                
+            }
+
+        } else if (operator1 === '') {
+
+            if (operatorSet === true) {
+                operator2 = 0;
+                return; 
+
+            } else {   
+                num2 = parseInt(displayValue);
+                operate(num1, num2, operator2);
+                updateDisplay();
+                num2 = '';
+                
+            };
+        };        
+    };
+
+};
+
+function updateDisplay() {
+    displayValue = result;
+    display.innerHTML = displayValue;
+    num1 = parseInt(displayValue);
+    operatorSet = true;
 };
 
 function operate(num1, num2, op) {
@@ -96,24 +133,26 @@ function operate(num1, num2, op) {
             break;
         default:
             alert('Select operator');
-    }
+    };
     
 };
 
+
+
+// Reset uniquement le display quand un nombre est tapé après l'enregistrement de l'operateur
 function clear() {
     displayValue = "";
     display.innerHTML = "";
     
-    console.log(`Num1 is ${num1}`);
-    console.log(`Num2 is ${num2}`);
-    console.log(`Operator1 is ${operator1}`);
-}
+};
 
+// Reset toutes les données pour repartir a 0.
 function clearButton() {
     displayValue = "";
     display.innerHTML = "";
     num1 = '';
     num2 = '';
     result = '';
-    operator1 = undefined;
-}
+    operator1 = '';
+    operator2 = '';
+};
